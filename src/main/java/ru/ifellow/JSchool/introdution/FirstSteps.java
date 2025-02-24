e9c233a4a8dedbf8068841e109ae079970945225
asharykin <aleksandr-sharykin@mail.ru>
2025-02-24 15:01:33 +0300

    Are you kidding me?

--- src/main/java/ru/ifellow/JSchool/introdution/FirstSteps.java
+++ src/main/java/ru/ifellow/JSchool/introdution/FirstSteps.java
@@ -77,7 +77,7 @@ public class FirstSteps {
 
     public boolean isSortedDescendant(int[] array) {
         for (int i = 0; i + 1 < array.length; i++) {
-            if (array[i] < array[i + 1]) {
+            if (array[i] <= array[i + 1]) {
                 return false;
             }
         }
